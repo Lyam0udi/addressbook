ec498b88a92937c7977b266212cad38511583e79
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-09-29 18:56:07 +0100

    Contact

---
+++ src/addressBookClasses/Contact.java
@@ -0,0 +1,10 @@
+package addressBookClasses;
+
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    // Constructor, getters, and setters
+}
+
