f8b59eb6ce2f1bd609ef1ba7e160a36fe0ad5113
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-09-29 18:58:07 +0100

    UserInterface

---
+++ src/addressBookClasses/AddressBook.java
@@ -0,0 +1,10 @@
+package addressBookClasses;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<Contact> contacts;
+
+    // Constructor and methods for adding, editing, deleting, and displaying contacts
+}
+
--- src/addressBookClasses/Main.java
+++ src/addressBookClasses/Main.java
@@ -3,11 +3,43 @@ package addressBookClasses;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-//        AddressBook addressBook = new AddressBook();
-//        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+	    AddressBook addressBook = new AddressBook();
+	    Scanner scanner = new Scanner(System.in);
+	    int choice;
 
-        // Implement user interface to add, edit, delete, and display contacts
-    }
+	    do {
+	        System.out.println("Address Book Menu:");
+	        System.out.println("1. Add Contact");
+	        System.out.println("2. Edit Contact");
+	        System.out.println("3. Delete Contact");
+	        System.out.println("4. Display All Contacts");
+	        System.out.println("5. Exit");
+	        System.out.print("Enter your choice: ");
+	        choice = scanner.nextInt();
+	        scanner.nextLine(); // Consume the newline character
+
+	        switch (choice) {
+	            case 1:
+	                // Logic to add a new contact
+	                break;
+	            case 2:
+	                // Logic to edit an existing contact
+	                break;
+	            case 3:
+	                // Logic to delete a contact
+	                break;
+	            case 4:
+	                // Logic to display all contacts
+	                break;
+	            case 5:
+	                System.out.println("Exiting the Address Book. Goodbye!");
+	                break;
+	            default:
+	                System.out.println("Invalid choice. Please try again.");
+	        }
+	    } while (choice != 5);
+	}
+
 }
 
