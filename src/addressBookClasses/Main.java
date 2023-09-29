12f8017765ac9a14eb8e5d6e1d7707ec4706622c
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-09-29 18:47:39 +0100

    Initialization project

--- src/addressBookClasses/Main.java
+++ src/addressBookClasses/Main.java
@@ -1,5 +1,13 @@
 package addressBookClasses;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+//        AddressBook addressBook = new AddressBook();
+//        Scanner scanner = new Scanner(System.in);
+
+        // Implement user interface to add, edit, delete, and display contacts
+    }
 }
+
