eedc6936f01312990e4c00d1ae5fbdd4f7cec05e
Lyam0udi <lyamoudi.ilias77@gmail.com>
2023-10-03 22:03:48 +0100

    Updates

--- src/addressBookClasses/AddressBookUI.java
+++ src/addressBookClasses/AddressBookUI.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
 
+
+
 public class AddressBookUI {
     private AddressBook addressBook;
     private ContactListView contactListView;
